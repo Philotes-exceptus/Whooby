eb7d4a9d6adcb831a6b57ad62043d7a10a7a9d80
Philotes-Friends <99619190+Philotes-Friends@users.noreply.github.com>
2022-04-28 00:46:34 +0530

    added a basic TTS project

---
+++ Whooby/app/src/main/java/com/example/whooby/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.whooby
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.util.*
+
+class MainActivity : AppCompatActivity(), TextToSpeech.OnInitListener {
+
+    lateinit var textToSpeech: TextToSpeech
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var btn: Button = findViewById(R.id.button)
+        var feed: EditText = findViewById(R.id.editText)
+        textToSpeech= TextToSpeech(this,this)
+        btn.setOnClickListener {
+            var text: String = feed.text.toString()
+            textToSpeech.speak(text,TextToSpeech.QUEUE_FLUSH,null)
+        }
+    }
+
+    override fun onInit(status: Int) {
+        if(status== TextToSpeech.SUCCESS)
+            {
+            val res : Int = textToSpeech.setLanguage(Locale.ENGLISH)
+                if(res==TextToSpeech.LANG_MISSING_DATA || res==TextToSpeech.LANG_NOT_SUPPORTED)
+                {
+                    Toast.makeText(this,"language not supported",Toast.LENGTH_LONG).show()
+                }
+                else
+                    {
+                        Toast.makeText(this,"initialize failed",Toast.LENGTH_LONG).show()
+                    }
+
+        }
+    }
+}
