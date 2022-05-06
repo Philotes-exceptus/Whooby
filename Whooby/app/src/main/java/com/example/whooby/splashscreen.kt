420fd0f4b4866bd8cf5d004dd0c09b82f5944d39
Swastik_Gorai <swastikgorai@gmail.com>
2022-05-06 14:18:33 +0530

    added a splash screen

---
+++ Whooby/app/src/main/java/com/example/whooby/splashscreen.kt
@@ -0,0 +1,30 @@
+package com.example.whooby
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+
+@Suppress("DEPRICATION")
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed({
+            val intent = Intent(this, opening::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+
+
+
+    }
+}
