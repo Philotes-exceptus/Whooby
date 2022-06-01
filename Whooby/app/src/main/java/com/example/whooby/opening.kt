02088ffe93948222ac47f033780375d52883a112
SySagar <sysagar07@gmail.com>
2022-06-01 16:06:33 +0530

    3d gltf model import successful

--- Whooby/app/src/main/java/com/example/whooby/opening.kt
+++ Whooby/app/src/main/java/com/example/whooby/opening.kt
@@ -66,4 +66,15 @@ class opening : AppCompatActivity() {
 
     }
 
+    fun anchor_whooby(view: View)
+    {
+
+        val intent3 = Intent(applicationContext, whooby::class.java)
+
+
+        if(view.getId()==R.id.whooby);
+        startActivity(intent3);
+
+    }
+
 }
