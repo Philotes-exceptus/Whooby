c9f18e70f56946d1a358134a6b16c2de619b8fc6
aniket3532 <aniketpradhan6047@gmail.com>
2022-06-13 01:09:58 +0530

    Login Signup for check

---
+++ Whooby/app/src/main/java/com/example/whooby/LoginActivity.kt
@@ -0,0 +1,36 @@
+package com.malkinfo.loginpageui
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.annotation.RequiresApi
+import kotlinx.android.synthetic.main.activity_main.*
+
+class LoginActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        singUp.setOnClickListener {
+            singUp.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            singUp.setTextColor(resources.getColor(R.color.textColor,null))
+            logIn.background = null
+            singUpLayout.visibility = View.VISIBLE
+            logInLayout.visibility = View.GONE
+            logIn.setTextColor(resources.getColor(R.color.pinkColor,null))
+        }
+        logIn.setOnClickListener {
+            singUp.background = null
+            singUp.setTextColor(resources.getColor(R.color.pinkColor,null))
+            logIn.background = resources.getDrawable(R.drawable.switch_trcks,null)
+            singUpLayout.visibility = View.GONE
+            logInLayout.visibility = View.VISIBLE
+            logIn.setTextColor(resources.getColor(R.color.textColor,null))
+        }
+        singIn.setOnClickListener {
+            startActivity(Intent(this@LoginActivity,opening::class.java))
+        }
+    }
+}
