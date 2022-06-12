c9b60ff4a72d45b6ceb71ecc61ec24e83995331b
Swastik Gorai <swastikgorai@gmail.com>
2022-06-12 08:53:45 +0530

    fetch from firebase 1

--- Whooby/app/src/main/java/com/example/whooby/ItemsViewModel.kt
+++ Whooby/app/src/main/java/com/example/whooby/ItemsViewModel.kt
@@ -1,4 +1,49 @@
 package com.example.whooby
 
-data class ItemsViewModel(val text: String)
-{}
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+data class ItemsViewModel(val text: String) : AppCompatActivity() {
+    lateinit var firebaseDatabase: FirebaseDatabase
+    lateinit var databaseReference: DatabaseReference
+    lateinit var retrieveTV: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance()
+        databaseReference = firebaseDatabase.getReference("InputText")
+        var retrieveTV = findViewById<TextView>(R.id.idTVRetrieveData)
+
+
+        getdata()
+
+    }
+
+    private fun getdata() {
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val post = dataSnapshot.getValue()
+
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        }
+        databaseReference.addValueEventListener((postListener))
+    }
+
+
+
+}
