dad196ea6c3b513b580ba383d92419c0ade40c0f
SySagar <sysagar07@gmail.com>
2022-10-31 21:59:41 +0530

    added port

--- Server/server.js
+++ Server/server.js
@@ -15,7 +15,7 @@ await mongoose.connect(dbURI, (err, db) => {
     else {
         console.log('connected to db')
         //listen for request
-        app.listen(process.env.PORT || 3000)
+        app.listen(process.env.api_key || 3000)
 
         run();
     }
